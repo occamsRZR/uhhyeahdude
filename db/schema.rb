0998da145e8ec24e5ece0459f166136fe5b4e5ff
vagrant <vagrant@seatbelts-berkshelf>
2014-08-14 05:56:34 +0000

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814055454) do
+ActiveRecord::Schema.define(version: 20140814055513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140814055454) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role"
   end
 
 end
